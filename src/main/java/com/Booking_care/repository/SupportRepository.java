7c7c0ee0c34c648e18089c0b16423350ec9fba08
VuVanDao <dao28901@gmail.com>
2025-09-11 10:22:07 +0700

    create supportRepository

---
+++ src/main/java/com/Booking_care/repository/SupportRepository.java
@@ -0,0 +1,9 @@
+package com.Booking_care.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Booking_care.domain.Support;
+
+public interface SupportRepository extends JpaRepository<Support,Long>{
+    
+}
