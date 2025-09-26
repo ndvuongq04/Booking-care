f0f5ad4697e678654a995acd0eccdffd1132681d
ndvuongq04 <nguyendinhvuong08122004@gmail.com>
2025-09-26 21:40:24 +0700

    add api public

--- src/main/java/com/Booking_care/config/SecurityConfig.java
+++ src/main/java/com/Booking_care/config/SecurityConfig.java
@@ -46,7 +46,10 @@ public class SecurityConfig {
                 "/",
                 "/api/v1/auth/login", "/api/v1/auth/refresh", "/api/v1/auth/register",
                 "/api/v1/auth/forgot-password-send-email", "/api/v1/auth/forgot-verify-otp",
-                "/api/v1/auth/forgot-password"
+                "/api/v1/auth/forgot-password",
+                "/api/v1/doctors", "/api/v1/doctors/**",
+                "/api/v1/clinics", "/api/v1/clinics/**",
+                "/api/v1/specialties", "/api/v1/specialties/**",
         };
         http
                 .csrf(c -> c.disable())
