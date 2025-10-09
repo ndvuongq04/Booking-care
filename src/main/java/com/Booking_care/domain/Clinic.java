38e7b060f94f9eea798326acacbe4651265c91e9
ndvuongq04 <nguyendinhvuong08122004@gmail.com>
2025-10-09 15:55:38 +0700

    fix clinic description

--- src/main/java/com/Booking_care/domain/Clinic.java
+++ src/main/java/com/Booking_care/domain/Clinic.java
@@ -3,6 +3,8 @@ package com.Booking_care.domain;
 import java.time.Instant;
 import java.util.List;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -30,6 +32,8 @@ public class Clinic {
     private long id;
 
     private String name;
+
+    @Column(columnDefinition = "MEDIUMTEXT")
     private String description;
 
     private String position; // vị trí (có thể để toạ độ hoặc text)
